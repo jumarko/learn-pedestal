8f3bdd38c1f15b456a6ac3d4c4ae0a01ddf633b2
Juraj Martinka <jumarko@gmail.com>
2022-05-20 09:24:05 +0200

    08 routes - add basic table-routes.

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,9 +1,55 @@
 (ns user
-  (:require [io.pedestal.http :as http]))
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
 
 (println "Loading user.clj")
 
 (defonce ^:private system-ref (atom nil))
+
+(defn list-recipes [request]
+  {:status 200
+   :body "list recipes"})
+
+(def table-routes
+  (route/expand-routes
+   #{{:app-name :cheffy :schema :http :host "learnpedestal.com"}
+     ;; now define the routes themselves
+     ["/recipes" :get list-recipes :route-name :list-recipes]}))
+;; if you inspect `table-routes` you will see this
+;; ({:path "/recipes",
+;;   :method :get,
+;;   :app-name :cheffy,
+;;   :path-re #"/\Qrecipes\E",
+;;   :path-parts ["recipes"],
+;;   :host "learnpedestal.com",
+;;   :interceptors
+;;   [{:name nil,
+;;     :enter #function[io.pedestal.interceptor/eval288/fn--289/fn--290],
+;;     :leave nil,
+;;     :error nil}],
+;;   :route-name :list-recipes,
+;;   :path-params []})
+
+;; add some more routes
+(defn upsert-recipe [request]
+  {:status 200
+   :body "upsert recipe"})
+
+(defn update-recipe [request]
+  {:status 200
+   :body "update recipe"})
+
+(def table-routes
+  (route/expand-routes
+   #{{:app-name :cheffy :schema :http :host "learnpedestal.com"}
+     ;; now define the routes themselves
+     ["/recipes" :get list-recipes :route-name :list-recipes]
+     ["/recipes" :post upsert-recipe :route-name :create-recipe]
+     ["/recipes/:recipe-id" :put update-recipe :route-name :update-recipe]
+     }))
+;; inspect `table-routes` again and notice `:path-params`:
+;;     :path-params [:recipe-id]
+
 
 (defn start-server []
   (->> {::http/routes #{}
