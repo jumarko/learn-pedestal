06225bd04503eac4555a081b31c2a0cf582b953f
Juraj Martinka <jumarko@gmail.com>
2022-06-08 06:51:11 +0200

    18 Create datomic schema and load seed data.
    We check whether there's any data in the db by performing a simple query
    and if there's nothing we create the schema and load the seed data.

--- src/dev/dev.clj
+++ src/dev/dev.clj
@@ -3,7 +3,8 @@
    [clojure.edn :as end]
    [cheffy.server :as server]
    [com.stuartsierra.component.repl :as cr]
-   [io.pedestal.http :as http]))
+   [io.pedestal.http :as http]
+   [datomic.client.api :as d]))
 
 (defn system [_old-system]
   (-> (slurp "src/config/development.edn")
@@ -32,7 +33,10 @@
   (keys cr/system)
 ;; => (:config :api-server :database)
 
-  ;; let's look at :api-server
+  .)
+
+;;; let's look at :api-server
+(comment
   (:api-server cr/system)
 
   (let [service-map (-> cr/system :api-server :service)
@@ -43,5 +47,36 @@
      (butlast default-interceptors)
      #_[my-interceptor]
      [(last default-interceptors)]))
+  )
+
+;;; check the database and create the schema and load data
+;;; but only if it's not already there
+(comment
+  (restart-dev)
+
+  ;; inspect the db quickly
+  (let [conn (-> cr/system :database :conn)
+        db (d/db conn)]
+    db)
+
+  ;; check if accounts exist already
+  (let [conn (-> cr/system :database :conn)
+        db (d/db conn)]
+    (d/pull db {:eid :account/account-id :selector '[*]}))
+  ;; => #:db{:id nil}
+
+  ;; ... go to database.clj and add `load-dataset` function
+  (restart-dev)
+  ;; ... now inspect the db again
+
+  (let [conn (-> cr/system :database :conn)
+        db (d/db conn)]
+    (d/q '[:find ?e ?id
+           :where [?e :account/account-id ?id]]
+         db))
+;; => [[79164837199977 "mike@mailinator.com"] [79164837199978 "jade@mailinator.com"] [79164837199976 "auth|5fbf7db6271d5e0076903601"] [79164837199979 "mark@mailinator.com"]]
 
   .)
+
+
+
